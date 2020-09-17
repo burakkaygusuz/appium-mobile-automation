77522ba53741631c1c4b32d1e4bd5dc565d4ac72
Burak Kaygusuz <kaygusuzburak@gmail.com>
2020-09-17 08:48:56 +0300

    A helper class was created to find points

---
+++ src/test/java/com/automation/utils/PointUtils.java
@@ -0,0 +1,27 @@
+package com.automation.utils;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.Dimension;
+
+public class PointUtils {
+
+    public static AppiumDriver<MobileElement> driver;
+
+    public PointUtils(AppiumDriver<MobileElement> driver) {
+        PointUtils.driver = driver;
+    }
+
+    public static int getPoint(double percentage) {
+        Dimension size = driver.manage().window().getSize();
+        return (int) (size.width * percentage);
+    }
+
+    public static int getXPoint(MobileElement element){
+        return element.getLocation().getX() + (element.getSize().getWidth() /2);
+    }
+
+    public static int getYPoint(MobileElement element){
+        return element.getLocation().getY() + (element.getSize().getHeight() /2);
+    }
+}
