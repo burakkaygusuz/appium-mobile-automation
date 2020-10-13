a4ce90ecfe03092da3a07268c05150b5dfa71965
Burak Kaygusuz <kaygusuzburak@gmail.com>
2020-10-13 11:43:47 +0300

    Bundle Id added

--- src/test/java/com/automation/config/DriverManager.java
+++ src/test/java/com/automation/config/DriverManager.java
@@ -48,6 +48,7 @@ public enum DriverManager implements DriverOptions {
             desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, props.getProperty("IOS_DEVICE_NAME"));
             desiredCapabilities.setCapability(MobileCapabilityType.UDID, "IOS_DEVICE_ID");
             desiredCapabilities.setCapability(MobileCapabilityType.APP, "IOS_APP_PATH");
+            desiredCapabilities.setCapability(IOSMobileCapabilityType.BUNDLE_ID, props.getProperty("BUNDLE_ID"));
             desiredCapabilities.setCapability(IOSMobileCapabilityType.LOCATION_SERVICES_ENABLED, true);
             desiredCapabilities.setCapability(IOSMobileCapabilityType.AUTO_ACCEPT_ALERTS, true);
 
