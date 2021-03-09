06dc9685821a10820a0fecfb4ac946dac9ccce1d
Burak Kaygusuz <kaygusuzburak@gmail.com>
2021-03-09 11:57:59 +0300

    Unnecessary assertions removed

--- src/test/java/com/automation/BaseTest.java
+++ src/test/java/com/automation/BaseTest.java
@@ -8,24 +8,20 @@ import io.appium.java_client.MobileElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 
-import java.io.IOException;
-
 public class BaseTest {
 
     private static final Logger log = LoggerFactory.getLogger(BaseTest.class);
     private final DesiredCapabilities capabilities = new DesiredCapabilities();
-    public AppiumDriver<MobileElement> driver;
+    protected AppiumDriver<MobileElement> driver;
 
     @BeforeSuite
-    public void startAppiumServer() throws IOException, InterruptedException {
+    public void startAppiumServer() throws Exception {
         ServerManager.startServer();
-        Assert.assertTrue(ServerManager.isServiceRunning(), "The appium server is not running!");
         log.info("The appium server is started...");
     }
 
@@ -46,7 +42,6 @@ public class BaseTest {
     @AfterSuite(alwaysRun = true)
     public void stopAppiumServer() {
         ServerManager.stopServer();
-        Assert.assertFalse(ServerManager.isServiceRunning(), "The appium server still running!");
         log.info("The appium server is stopped...");
     }
 
