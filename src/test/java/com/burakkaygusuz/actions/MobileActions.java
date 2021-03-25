17857c708b7b94aa82c4287b0a22b856f84848c9
Burak Kaygusuz <kaygusuzburak@gmail.com>
2021-03-25 08:28:37 +0300

    Refactor the mobile actions

--- src/test/java/com/burakkaygusuz/actions/MobileActions.java
+++ src/test/java/com/burakkaygusuz/actions/MobileActions.java
@@ -1,58 +1,87 @@
 package com.burakkaygusuz.actions;
 
-import com.burakkaygusuz.utils.PointUtils;
+import com.burakkaygusuz.enums.Directions;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.TapOptions;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.ElementOption;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.Dimension;
 
-import static com.burakkaygusuz.utils.ActionUtils.*;
+import java.time.Duration;
 
 @SuppressWarnings("rawtypes")
 public class MobileActions {
 
     private final AppiumDriver<MobileElement> driver;
-    private final PointUtils pointUtils;
 
     public MobileActions(AppiumDriver<MobileElement> driver) {
         this.driver = driver;
-        pointUtils = new PointUtils(driver);
     }
 
-    public TouchAction tapByCoordinates(int x, int y) {
-        return getTapAction(driver, x, y, 250);
+    public TouchAction singleTapByElement(MobileElement element, long milliSeconds) {
+        return new TouchAction(driver).tap(new TapOptions()
+                .withElement(ElementOption.element(element)))
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(milliSeconds)))
+                .release().perform();
+    }
+
+    public TouchAction singleTapByCoordinates(int x, int y, long milliSeconds) {
+        return new TouchAction(driver).tap(PointOption.point(x, y))
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(milliSeconds)))
+                .release().perform();
+    }
+
+    public TouchAction doubleTapByElement(MobileElement element, long milliSeconds) {
+        return new TouchAction(driver).tap(new TapOptions().withTapsCount(2).withElement(ElementOption.element(element)))
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(milliSeconds)))
+                .release().perform();
     }
 
     public TouchAction pressByElement(MobileElement element, long seconds) {
-        return getPressAction(driver, element, seconds);
+        return new TouchAction(driver).press(ElementOption.element(element))
+                .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(seconds)))
+                .release().perform();
     }
 
     public TouchAction pressByCoordinates(int x, int y, long seconds) {
-        return getPressAction(driver, x, y, seconds);
+        return new TouchAction(driver).press(PointOption.point(x, y))
+                .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(seconds)))
+                .release().perform();
     }
 
-    public TouchAction horizontalSwipeByPercentage(double startPercentage, double endPercentage, double anchorPercentage) {
-        int anchor = pointUtils.getPoint(anchorPercentage);
-        int startPoint = pointUtils.getPoint(startPercentage);
-        int endPoint = pointUtils.getPoint(endPercentage);
+    public TouchAction swipeScreen(Directions direction) {
 
-        return getSwipeAction(driver, anchor, startPoint, endPoint, 1000);
+        PointOption pointOptionStart, pointOptionEnd;
+        Dimension dimension = driver.manage().window().getSize();
+
+        pointOptionStart = PointOption.point(dimension.width / 2, dimension.height / 2);
+
+        int edgeBorder = 10;
+
+        switch (direction) {
+            case DOWN:
+                pointOptionEnd = PointOption.point(dimension.width / 2, dimension.height - edgeBorder);
+                break;
+            case UP:
+                pointOptionEnd = PointOption.point(dimension.width / 2, edgeBorder);
+                break;
+            case LEFT:
+                pointOptionEnd = PointOption.point(edgeBorder, dimension.height / 2);
+                break;
+            case RIGHT:
+                pointOptionEnd = PointOption.point(dimension.width - edgeBorder, dimension.height / 2);
+                break;
+            default:
+                throw new IllegalArgumentException("swipeScreen(): dir: '" + direction + "' not supported");
+        }
+
+        return new TouchAction(driver).press(pointOptionStart)
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(200)))
+                .moveTo(pointOptionEnd)
+                .release().perform();
     }
 
-    public TouchAction verticalSwipeByPercentages(double startPercentage, double endPercentage, double anchorPercentage) {
-        int anchor = pointUtils.getPoint(anchorPercentage);
-        int startPoint = pointUtils.getPoint(startPercentage);
-        int endPoint = pointUtils.getPoint(endPercentage);
-
-        return getSwipeAction(driver, anchor, startPoint, endPoint, 1000);
-    }
-
-    public TouchAction swipeByElements(MobileElement startElement, MobileElement endElement) {
-        int startX = pointUtils.getXPoint(startElement);
-        int startY = pointUtils.getYPoint(startElement);
-
-        int endX = pointUtils.getXPoint(endElement);
-        int endY = pointUtils.getYPoint(endElement);
-
-        return getSwipeAction(driver, startX, startY, endX, endY, 1000);
-    }
 }
